1bd1842ff71443053adb378515909e35285be857
Chris Cornutt <chris.cornutt@hp.com>
2014-10-09 11:02:43 -0500

    moving the channel end tag to the rigth place

--- templates/feed.php
+++ templates/feed.php
@@ -7,14 +7,14 @@
 		<language>en-us</language>
 		<pubDate><?php echo date('r'); ?></pubDate>
 		<ttl>30</ttl>
+		<?php foreach ($items as $item): ?>
+		<item>
+			<title><?php echo $item['name']; ?> (<?php echo $item['version']; ?>)</title>
+			<guid><?php echo $item['source']; ?></guid>
+			<link><?php echo $item['source']; ?></link>
+			<description><![CDATA[<?php echo $item['description']; ?>]]></description>
+			<pubDate><?php echo $item['date_posted']; ?> UTC</pubDate>
+		</item>
+		<?php endforeach; ?>
 	</channel>
-	<?php foreach ($items as $item): ?>
-	<item>
-		<title><?php echo $item['name']; ?> (<?php echo $item['version']; ?>)</title>
-		<guid><?php echo $item['source']; ?></guid>
-		<link><?php echo $item['source']; ?></link>
-		<description><![CDATA[<?php echo $item['description']; ?>]]></description>
-		<pubDate><?php echo $item['date_posted']; ?> UTC</pubDate>
-	</item>
-	<?php endforeach; ?>
 </rss>
