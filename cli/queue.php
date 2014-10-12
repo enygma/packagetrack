938686d161fcb74d0f400ac8cf65d4554d9a4c20
Chris Cornutt <enygma@phpdeveloper.org>
2014-10-12 08:20:55 -0500

    fixing include paths

--- cli/queue.php
+++ cli/queue.php
@@ -1,8 +1,6 @@
 <?php
-error_log('here');
-
-require_once '../vendor/autoload.php';
-require_once '../PackageScan.php';
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../PackageScan.php';
 
 // use FastFeed\Factory;
 use Aura\Sql\ExtendedPdo;
